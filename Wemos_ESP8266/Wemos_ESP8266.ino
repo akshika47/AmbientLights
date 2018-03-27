caa39c672abea4d5269b7073cd2e89eba803bb7d
mct1234 <mct1234@mct1234s-MacBook-Pro.local>
2018-03-27 14:15:50 +0100

    Power up the neo pixels and  commented wifi connection for testing purpose

--- Wemos_ESP8266/Wemos_ESP8266.ino
+++ Wemos_ESP8266/Wemos_ESP8266.ino
@@ -1,14 +1,21 @@
 #include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h>
+
 
 //SSID of your network
 char ssid[] = ""; //SSID of your Wi-Fi router
 char pass[] = ""; //Password of your Wi-Fi router
+int delayval = 500; // delay for half a second
+int LED = D5;
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, LED , NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
   Serial.begin(115200);
-  delay(10);
-
+  ring.begin();
+  ring.setBrightness(30); //adjust brightness here
+  ring.show();
+/*
   // Connect to Wi-Fi network
   Serial.println();
   Serial.println();
@@ -23,7 +30,20 @@ void setup()
   }
   Serial.println("");
   Serial.println("Wi-Fi connected successfully");
+*/
 }
 
-void loop () {}
+
+void loop () {  
+  for(int i=0;i<12;i++){
+    Serial.println("Inside the loop");
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    ring.setPixelColor(i, ring.Color(0,150,0)); // Moderately bright green color.
+
+    ring.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+  }
 
