8be63f25676fcab9c16c6e07f49267d17c6f443e
Akshika47 <akshikaakalanka@gmail.com>
2018-03-28 19:43:02 +0100

    Added Adafruit MQTT library and setup to controlled via MQTT broker over the wifi

--- Wemos_ESP8266/Wemos_ESP8266.ino
+++ Wemos_ESP8266/Wemos_ESP8266.ino
@@ -1,11 +1,35 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_NeoPixel.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 
+/************************* WiFi Access Point *********************************/
 
-//SSID of your network
 char ssid[] = ""; //SSID of your Wi-Fi router
 char pass[] = ""; //Password of your Wi-Fi router
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+
+
 int delayval = 500; // delay for half a second
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+void MQTT_connect();
 
 int LED = D5;
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, LED , NEO_GRB + NEO_KHZ800);
@@ -17,9 +41,6 @@ void setup()
   ring.setBrightness(30); //adjust brightness here
   ring.show(); //make the neo pixels show the configuration
 
-  
-/*
-  //Uncomment the code snipet below to activate the wifi connection
   // Connect to Wi-Fi network
   Serial.println();
   Serial.println();
@@ -34,20 +55,91 @@ void setup()
   }
   Serial.println("");
   Serial.println("Wi-Fi connected successfully");
-*/
+
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&onoffbutton);
+
 }
 
 
-void loop () {  
-  for(int i=0;i<12;i++){
-    Serial.println("Inside the loop");
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    ring.setPixelColor(i, ring.Color(0,150,0)); // Moderately bright green color.
+void loop () {
 
-    ring.show(); // This sends the updated pixel color to the hardware.
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
 
-    delay(delayval); // Delay for a period of time (in milliseconds).
+ 
+  
+ // this is our 'wait for incoming subscription packets' busy subloop
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    Serial.println("Inside the loop1");
+    if (subscription == &onoffbutton) {
+      Serial.println("Inside the loop2");
+
+      String str((char *)onoffbutton.lastread);
+      if (str.equals("green"))
+      {
+        for (int i = 0; i < 12; i++) {
+          Serial.println("Inside the loop3");
+          // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+          ring.setPixelColor(i, ring.Color(0, 150, 0)); // Moderately bright green color.
+
+          ring.show(); // This sends the updated pixel color to the hardware.
+
+          delay(delayval); // Delay for a period of time (in milliseconds).
+
+        }
+      }
+      if (str.equals("blue"))
+      {
+        for (int i = 0; i < 12; i++) {
+          Serial.println("Inside the loop3");
+          // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+          ring.setPixelColor(i, ring.Color(0, 0, 150)); // Moderately bright blue color.
+
+          ring.show(); // This sends the updated pixel color to the hardware.
+
+          delay(delayval); // Delay for a period of time (in milliseconds).
+
+        }
+
+        Serial.print(F("Got: "));
+        Serial.println((char *)onoffbutton.lastread);
+      }
+    }
+
+
 
   }
+
+}
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
   }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);  // wait 5 seconds
+    retries--;
+    if (retries == 0) {
+      // basically die and wait for WDT to reset me
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+}
 
