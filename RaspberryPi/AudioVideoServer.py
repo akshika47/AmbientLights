2cf2f48d85a12f19264e332da4d3c85a286bcfdb
Akshika47 <akshikaakalanka@gmail.com>
2018-04-24 17:48:35 +0100

    added code for string comparison

--- RaspberryPi/AudioVideoServer.py
+++ RaspberryPi/AudioVideoServer.py
@@ -4,8 +4,8 @@ import os
 
 HOST = 'io.adafruit.com'
 PORT = 1883
-USERNAME = 'akshika47'
-PASSWORD = '95c18e999ffc4bf79c62ef42c812fcc7'
+USERNAME = ''
+PASSWORD = ''
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -20,10 +20,39 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    os.system('omxplayer -o hdmi 1.MP4')
+    command = str(msg.payload);
+    
+    if(command == "1a")
+    	os.system('omxplayer -o hdmi 1.mp3')
 
+    if(command == "2a")
+    	os.system('omxplayer -o hdmi 2.mp3')
 
+    if(command == "3a")
+    	os.system('omxplayer -o hdmi 3.mp3')
 
+    if(command == "4a")
+    	os.system('omxplayer -o hdmi 4.mp3')
+
+    if(command == "5a")
+    	os.system('omxplayer -o hdmi 5.mp3')
+
+    if(command == "1v")
+    	os.system('omxplayer -o hdmi 1.MP4')
+
+    if(command == "2v")
+    	os.system('omxplayer -o hdmi 2.MP4')
+
+    if(command == "3v")
+    	os.system('omxplayer -o hdmi 3.MP4')
+
+    if(command == "4v")
+    	os.system('omxplayer -o hdmi 4.MP4')
+
+    if(command == "5v")
+    	os.system('omxplayer -o hdmi 5.MP4')
+
+ 
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
 client.connect(host=HOST, port=PORT)
