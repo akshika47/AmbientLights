3305c1eed41ca0364377b04f7c88bb20fe104068
Akshika47 <akshikaakalanka@gmail.com>
2018-05-10 22:26:38 +0100

    added changes to the Python server running on the Pi

--- RaspberryPi/AudioVideoServer.py
+++ RaspberryPi/AudioVideoServer.py
@@ -20,48 +20,53 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    command = str(msg.payload);
+    command = str(msg.payload)
     
-    if(command == "1a")
-    	os.system('omxplayer -o hdmi 1.mp3')
+    if(command == "1a"):
+        os.system('omxplayer -o hdmi 1.wav')
 
-    if(command == "2a")
-    	os.system('omxplayer -o hdmi 2.mp3')
+    if(command == "2a"):
+        os.system('omxplayer -o hdmi 2.wav')
 
-    if(command == "3a")
-    	os.system('omxplayer -o hdmi 3.mp3')
+    if(command == "3a"):
+        os.system('omxplayer -o hdmi 3.wav')
 
-    if(command == "4a")
-    	os.system('omxplayer -o hdmi 4.mp3')
+    if(command == "4a"):
+        os.system('omxplayer -o hdmi 4.wav')
 
-    if(command == "5a")
-    	os.system('omxplayer -o hdmi 5.mp3')
+    if(command == "5a"):
+        os.system('omxplayer -o hdmi 5.wav')
 
-    if(command == "1v")
-    	os.system('omxplayer -o hdmi 1.MP4')
+    if(command == "6a"):
+        os.system('omxplayer -o hdmi 6.wav')
 
-    if(command == "2v")
-    	os.system('omxplayer -o hdmi 2.MP4')
+    if(command == "1v"):
+        os.system('sudo fbi -T 2 1.png')
 
-    if(command == "3v")
-    	os.system('omxplayer -o hdmi 3.MP4')
+    if(command == "2v"):
+        os.system('sudo fbi -T 2 2.png')
 
-    if(command == "4v")
-    	os.system('omxplayer -o hdmi 4.MP4')
+    if(command == "3v"):
+        os.system('sudo fbi -T 2 3.png')
 
-    if(command == "5v")
-    	os.system('omxplayer -o hdmi 5.MP4')
+    if(command == "4v"):
+        os.system('sudo fbi -T 2 4.png')
 
- 
+    if(command == "5v"):
+        os.system('sudo fbi -T 2 5.jpeg')
+    
+    if(command == "61v"):
+        os.system('sudo fbi -T 2 61.png')
+
+    if(command == "62v"):
+        os.system('sudo fbi -T 2 62.png')
+
+
+
 client = mqtt.Client()
 client.username_pw_set(USERNAME, PASSWORD)
 client.connect(host=HOST, port=PORT)
 client.on_connect = on_connect
 client.on_message = on_message
 
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 client.loop_forever()
